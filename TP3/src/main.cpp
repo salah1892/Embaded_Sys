9ab46ed7163c8169a53d0fe07e4b7aa61918c6fa
salah1892 <86801938+salah1892@users.noreply.github.com>
2022-10-18 17:09:42 +0100

    Update main.cpp

--- TP3/src/main.cpp
+++ TP3/src/main.cpp
@@ -2,36 +2,13 @@
 //#include<Timer.h>
 #include "configBIN.h"
 
-// hw_timer_t *My_timer = NULL;
-// hw_timer_t *My_timer2 = NULL;
 hw_timer_t *Timer_Led_Clignote = NULL;
 
 volatile int count = MAX_LOW_LED;
 volatile int count_Second = 5;
 volatile int count_Half_Second = 3;
 volatile int CONT_BTN_PRESSED = 0;
-portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
-//---------------------------------------------------//
-// void IRAM_ATTR onTimer1()
-// {
-//   digitalWrite(LED1, !digitalRead(LED1));
-//   // delay(1000);
-//   // digitalWrite(LED1, LOW);
-// }
-// void IRAM_ATTR onTimer2()
-// {
-//   count--;
-//   if (count == 0)
-//   {
-//     digitalWrite(LED2, ON);
-//     count = MAX_LOW_LED;
-//   }
-//   else
-//   {
-//     digitalWrite(LED2, OFF);
-//   }
-// }
-//-------------------------------------------------------------------//
+
 void clignote_1s()
 {
   count_Second--;
@@ -45,7 +22,7 @@ void clignote_1s()
     digitalWrite(LED1, OFF);
   }
 }
-//-----------------------------------//
+//---------------------------------------------------------------//
 void clignote_Half_sec()
 {
   count_Half_Second--;
@@ -59,7 +36,7 @@ void clignote_Half_sec()
     digitalWrite(LED1, OFF);
   }
 }
-//-----------------------------------//
+//---------------------------------------------------------------//
 void IRAM_ATTR onTimer3()
 {
   if (digitalRead(BUTTON_1) == ON)
@@ -88,7 +65,6 @@ void IRAM_ATTR onTimer3()
   }
 }
 
-//---------------------------------------------------//
 void setup()
 {
   pinMode(BUTTON_1, INPUT_PULLDOWN);
@@ -98,16 +74,7 @@ void setup()
   digitalWrite(LED1, LOW);
   digitalWrite(LED2, LOW);
   Serial.begin(115200);
-  // My_timer = timerBegin(0, 80, true);
-  // timerAttachInterrupt(My_timer, &onTimer1, true);
-  // timerAlarmWrite(My_timer, SECOND, true);
-  // timerAlarmEnable(My_timer); // Just Enable Timer
-
-  // My_timer2 = timerBegin(1, 80, true);
-  // timerAttachInterrupt(My_timer2, &onTimer2, true);
-  // timerAlarmWrite(My_timer2, HALF_SECOND, true);
-  // timerAlarmEnable(My_timer2); // Just Enable Timer
-  //--------------------------------------------------------//
+  
   Timer_Led_Clignote = timerBegin(0, 80, true);
   timerAttachInterrupt(Timer_Led_Clignote, &onTimer3, true);
   timerAlarmWrite(Timer_Led_Clignote, QUARD_SECOND, true);
